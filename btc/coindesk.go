ca56911d6838c07ee2468b81643608787f53ed3c
Chetan Sarva <chetan@pixelcop.net>
2017-09-12 16:53:08 -0400

    split out coindesk.go

---
+++ btc/coindesk.go
@@ -0,0 +1,50 @@
+package btc
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type CoindeskSource struct {
+	Bpi struct {
+		USD struct {
+			RateFloat float64 `json:"rate_float"`
+		} `json:"USD"`
+		EUR struct {
+			RateFloat float64 `json:"rate_float"`
+		} `json:"EUR"`
+	} `json:"bpi"`
+}
+
+func (s *CoindeskSource) GetUSD() float64 {
+	return s.Bpi.USD.RateFloat
+}
+
+func (s *CoindeskSource) GetEUR() float64 {
+	return s.Bpi.EUR.RateFloat
+}
+
+func (s *CoindeskSource) Update() error {
+
+	client := http.Client{
+		Timeout: 60 * time.Second,
+	}
+
+	resp, err := client.Get("https://api.coindesk.com/v1/bpi/currentprice.json")
+	if err != nil {
+		return fmt.Errorf("Failed to fetch from coindesk: %v", err)
+	}
+	defer resp.Body.Close()
+
+	src := CoindeskSource{}
+	err = json.NewDecoder(resp.Body).Decode(&src)
+	if err != nil {
+		return fmt.Errorf("Unexpected Response: %v", err)
+	}
+
+	s.Bpi = src.Bpi
+
+	return nil
+}
--- btc/plugin.go
+++ btc/plugin.go
@@ -1,10 +1,7 @@
 package btc
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"time"
 
 	"github.com/go-chat-bot/bot"
 )
@@ -12,48 +9,6 @@ import (
 var (
 	price = CoindeskSource{}
 )
-
-type CoindeskSource struct {
-	Bpi struct {
-		USD struct {
-			RateFloat float64 `json:"rate_float"`
-		} `json:"USD"`
-		EUR struct {
-			RateFloat float64 `json:"rate_float"`
-		} `json:"EUR"`
-	} `json:"bpi"`
-}
-
-func (s *CoindeskSource) GetUSD() float64 {
-	return s.Bpi.USD.RateFloat
-}
-
-func (s *CoindeskSource) GetEUR() float64 {
-	return s.Bpi.EUR.RateFloat
-}
-
-func (s *CoindeskSource) Update() error {
-
-	client := http.Client{
-		Timeout: 60 * time.Second,
-	}
-
-	resp, err := client.Get("https://api.coindesk.com/v1/bpi/currentprice.json")
-	if err != nil {
-		return fmt.Errorf("Failed to fetch from coindesk: %v", err)
-	}
-	defer resp.Body.Close()
-
-	src := CoindeskSource{}
-	err = json.NewDecoder(resp.Body).Decode(&src)
-	if err != nil {
-		return fmt.Errorf("Unexpected Response: %v", err)
-	}
-
-	s.Bpi = src.Bpi
-
-	return nil
-}
 
 // SetChannels enables the BTC cron for the given list of channels
 func SetChannels(channels []string) {
